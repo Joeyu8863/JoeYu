c05d51a6958bd1dd1fc1d9bdffe348af6366c6cd
cs130aJoeYu <57922509+cs130aJoeYu@users.noreply.github.com>
2019-11-20 15:51:00 -0800

    Update GraphAnalyzer.cpp
    I finsih the logic of topKNeighbors function and add addtional sort function to make it work. But I still not sure about  a lot of empty slots would affect it or not. And the feature also have some problems when constructing graph, can u check what's going on there?

--- GraphAnalyzer.cpp
+++ GraphAnalyzer.cpp
@@ -34,14 +34,57 @@ string GraphAnalyzer::topKOpenTriangles(int k) {
 
 vector<int> GraphAnalyzer::topKNeighbors(int nodeID, int k,  vector<float> w) {
     //TODO
-    vector<int>list;
+    vector<vector<int>> list;
+    int index = 0;
+    int id = 0;
+    for (int i = 0; i<G.hashtableedge[nodeID].size(); i++) {//access every edge contains nodeid
+        if (G.hashtableedge[nodeID][i]->IdA == nodeID) {// if ida in the edge is nodeid, then idb is the search target
+            //question would empty G.hashtableedge[nodeID][i] break this line?
+            id = G.hashtableedge[nodeID][i]->IdB;
+        }
+        else if (G.hashtableedge[nodeID][i]->IdB == nodeID){// if idb in the edge is nodeid, then idb is the search target
+            id = G.hashtableedge[nodeID][i]->IdA;
+            
+            }
     
-    for (int i = 0; i<G.nnode; i++) {
-        list.insert(<#const_iterator __position#>, <#const_reference __x#>)
+            int score = 0;//initial score
+            for (int j = 0; j<G.hashtable[id]->features.size(); j++) { // compute the score of neighbor id
+                score += w[j]*G.hashtable[id]->features[j];
+            }
+            if (list.size()<k && score!=0) {// if the list have less than k items just add it
+                list[index][0]= G.hashtable[i]->id;
+                list[index][1]= score;
+                //then sort it
+                sortlist(index+1, list);
+            }
+            else// if list have k items, replace the smallest one
+            {
+                
+                
+                    if (score > list[k-1][1]) {
+                        list[index][0]= G.hashtable[i]->id;
+                        list[index][1]= score;
+                    }
+                sortlist(k, list); // sort list
+                
+            }
+            
+        }
+    vector<int> result;
+    int size = 0;
+    if (k>index+1) { // take the min of k & neighbors
+        size = index +1;
     }
+    else
+        size = k;
+    for (int l = 0; l < size ; l++) { //move ordered id to vector result and return
+        result[l]=list[l][1];
+    }
+        
     
     
-    return vector<int> {3};
+    return result;
+    //return vector<int> {3};
 };
 
 
@@ -55,6 +98,25 @@ float GraphAnalyzer::jacardIndexOfTopKNeighborhoods(int nodeAID, int nodeBiID, i
     //TODO
     return 0;
 };
+void GraphAnalyzer::sortlist(int k, vector<vector<int>> list){
+    for (int i = 0; i < k; i++) {
+        for (int j = 1; j < k; j++) {
+            if (list[i][1]<list[j][1]) {
+                int idtemp = list[i][0];
+                int scoretemp = list[i][1];
+                list[i][0] = list[j][0];
+                list[i][1] = list[j][1];
+                list[j][0] = idtemp;
+                list[j][1] = scoretemp;
+            }
+        }
+        
+    }
+    return;
+    
+};
+
+
 
 
 
